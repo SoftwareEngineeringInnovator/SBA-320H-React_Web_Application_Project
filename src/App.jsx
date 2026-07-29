7d4ff352f78c45815a7ec82d5f2b9ec2b90cb154
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-29 18:11:19 -0400

    imported the header component to the App.jsx file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      {/* Display the Cybersecurity Application to search for vulnerabilities */}
+      <Header />
+    </main>
+  );
+}
+
+export default App;
