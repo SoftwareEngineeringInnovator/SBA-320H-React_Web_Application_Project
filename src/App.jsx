4b5f8d19d8324a6ab6871a4443f838d325d5ce3b
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-29 13:47:44 -0400

    created h1 and p elements to check if page is connected correctly in the browser

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      {/* Cybersecurity Application to search for vulnerabilities */}
+      <header>
+        <h1>Cybersecurity Vulnerability Explorer</h1>
+
+        <p>
+          Search the National Vulnerability Database for publicly reported
+          cybersecurity vulnerabilities.
+        </p>
+      </header>
+    </main>
+  );
+}
+
+export default App;
