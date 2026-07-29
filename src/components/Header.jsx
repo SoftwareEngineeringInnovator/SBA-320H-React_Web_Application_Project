9ee0ca283bfcfbc07544c73d9d7b59f8ed0d65ce
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-29 17:59:12 -0400

    created Header.jsx file to render the Header information in the App.jsx file

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header>
+      {/* Cybersecurity Application to search for vulnerabilities */}
+      <h1>Cybersecurity Vulnerability Explorer</h1>
+
+      <p>
+        Search the National Vulnerability Database for publicly reported
+        cybersecurity vulnerabilities.
+      </p>
+    </header>
+  );
+}
+
+export default Header;
