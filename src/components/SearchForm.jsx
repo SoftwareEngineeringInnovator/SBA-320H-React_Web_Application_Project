e49a2f6bd397798383b54568e542b37585e5bd68
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-29 20:54:36 -0400

    added useState and modified the code by adding serachTerm, setSerachTerm to allow user to serach for a vulnerability

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,19 +1,29 @@
+import { useState } from "react";
+
 function SearchForm() {
-  return (
-    <section className="search-section">
-      <h2>Search for Vulnerabilities</h2>
 
-      <form>
-        {/* The label explains what information the user should enter */}
-        
-        <label htmlFor="search-input">Enter a product, technology, or CVE identification number: </label>
+    // Store the text entered in the search input
+  const [searchTerm, setSearchTerm] = useState("");
 
-        <input id="search-input" type="text" placeholder="Example: Windows or CVE-2021-44228"/>
+    return (
+        <section className="search-section">
+            <h2>Search for Vulnerabilities</h2>
 
-        <button type="submit">Search</button>
-      </form>
-    </section>
-  );
+            <form>
+                {/* The label explains what information the user should enter */}
+
+                <label htmlFor="search-input">Enter a product, technology, or CVE identification number: </label>
+
+                <input id="search-input" type="text" placeholder="Example: Windows or CVE-2021-44228"
+                    value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} />
+
+                <button type="submit">Search</button>
+            </form>
+
+            {/* Temporarily display the entered text to test useState */}
+            <p>You are searching for: {searchTerm}</p>
+        </section>
+    );
 }
 
 export default SearchForm;
