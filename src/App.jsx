985c584188043412dee9866c82eb584382b3f947
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-29 22:04:57 -0400

    connected the searchReducer component to App.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Header from "./components/Header";
 import SearchForm from "./components/SearchForm";
+import { useReducer } from "react";
+import { initialSearchState, searchReducer, } from "./reducers/searchReducer";
 import "./App.css";
 
 function App() {
+  // Manage the vulnerability search information with useReducer
+  const [searchState, dispatch] = useReducer(searchReducer, initialSearchState,);
 
-    // Receive the search term submitted by the SearchForm component.
+  // Receive the search term submitted by the SearchForm component
   function handleSearch(searchTerm) {
     console.log("Search received by App:", searchTerm);
+
+    dispatch({ type: "SEARCH_STARTED" });
   }
 
   return (
@@ -16,6 +22,9 @@ function App() {
 
       {/* Display the vulnerability search form */}
       <SearchForm onSearch={handleSearch} />
+
+      {/* Temporarily confirm that the reducer received the action */}
+      {searchState.loading && <p>Preparing vulnerability search...</p>}
     </main>
   );
 }
