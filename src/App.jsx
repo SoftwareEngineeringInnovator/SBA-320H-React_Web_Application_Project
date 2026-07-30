88bdc09e2820223b0cd50330bf93ac1c9120d889
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-29 21:14:25 -0400

    added function handleSearch to file App.jsx to receive the searched information

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import SearchForm from "./components/SearchForm";
 import "./App.css";
 
 function App() {
+
+    // Receive the search term submitted by the SearchForm component.
+  function handleSearch(searchTerm) {
+    console.log("Search received by App:", searchTerm);
+  }
+  
   return (
     <main>
       {/* Display the Cybersecurity Application to search for vulnerabilities */}
